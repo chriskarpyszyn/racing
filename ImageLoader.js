384a1d4230d2b5457dad002a86849e78865cbea1
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2016-04-07 23:14:28 -0400

    Improve ImageLoader. Move images to image folder.

--- ImageLoader.js
+++ ImageLoader.js
@@ -1,15 +1,25 @@
 var carPic = document.createElement("img");
 var trackRoadPic = document.createElement("img");
 var trackWallPic = document.createElement("img");
-var picsToLoad = 3;
+var picsToLoad = 0;
 
 function loadImages() {
-    carPic.onload=countLoadedImagesAndLaunch;
-    carPic.src="player1.png";
-    trackRoadPic.onload=countLoadedImagesAndLaunch;
-    trackRoadPic.src="track_road.png";
-    trackWallPic.onload=countLoadedImagesAndLaunch;
-    trackWallPic.src="track_wall.png";
+    var imageList = [
+        { varName: carPic, fileName: "player1.png" },
+        { varName: trackRoadPic, fileName: "track_road.png" },
+        { varName: trackWallPic, fileName: "track_wall.png" }
+    ];
+
+    picsToLoad = imageList.length;
+
+    for (var i = 0; i < imageList.length; i++) {
+        beginLoadingImage(imageList[i].varName, imageList[i].fileName);
+    }
+}
+
+function beginLoadingImage(imgVar, fileName) {
+    imgVar.onload = countLoadedImagesAndLaunch;
+    imgVar.src = "images/"+fileName;
 }
 
 function countLoadedImagesAndLaunch() {
